de05b368aae18bf0b8118ac7fdad159ee591b888
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-08 00:42:20 -0500

    creacion de interfaces de clases cobranza y tecnico

--- src/main/java/javafxbase/App.java
+++ src/main/java/javafxbase/App.java
@@ -35,4 +35,6 @@ public class App extends Application {
         launch();
     }
 
+    
+    
 }
--- src/main/java/modelo/Proveedor.java
+++ src/main/java/modelo/Proveedor.java
@@ -1,6 +1,11 @@
 
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Proveedor extends Persona{
 
     public Proveedor(String codigo, String nombre, String direccion, String telefono) {
@@ -10,6 +15,21 @@ public class Proveedor extends Persona{
     @Override
     public String toString() {
         return super.toString();
+    }
+    
+    public static ArrayList<Proveedor> cargarLista(){
+        ArrayList<Proveedor> proveedores = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/proveedores.txt"));){
+            br.readLine();
+            String linea;
+            while ((linea = br.readLine())!=null){
+                String[] datos = linea.split(","); 
+                proveedores.add(new Proveedor(datos[0],datos[1],datos[2],datos[3]));
+            }
+        }catch (IOException e){
+            
+        }
+        return proveedores;
     }
     
 }
