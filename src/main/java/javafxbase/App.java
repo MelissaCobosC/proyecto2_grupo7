def06f52804644e49ed4ac00b8c440e5c3dc4547
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-08 15:33:27 -0500

    arreglando interfaces

--- src/main/java/javafxbase/App.java
+++ src/main/java/javafxbase/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("reportarIngresos"), 380, 400);
+        scene = new Scene(loadFXML("iniciaSesion"), 400, 400);
         stage.setScene(scene);
         stage.show();
     }
