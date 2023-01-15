ba1823d38db2863d5c8d344a3fd6a1fadaee18a9
carmi <carmi@192.168.1.112>
2023-01-15 13:17:12 -0500

    disenio menu admin

---
+++ src/main/java/javafxbase/AdministradorController.java
@@ -0,0 +1,54 @@
+
+package javafxbase;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author carmi
+ */
+public class AdministradorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    
+    @FXML
+    private Button btnclientes;
+    @FXML
+    private Button btnproovedores;
+    @FXML
+    private Button btnservidores;
+    
+    
+    
+    @FXML
+    private void administrarClientes(ActionEvent event) throws IOException {
+    App.setRoot("consultarOrden");
+    }
+
+    @FXML
+    private void administrarServicios(ActionEvent event) throws IOException {
+    App.setRoot("generarOrden");
+    }
+
+    @FXML
+    private void administrarProovedores(ActionEvent event) throws IOException {
+    App.setRoot("reportarInsumo");
+    }
+   
+    
+}
