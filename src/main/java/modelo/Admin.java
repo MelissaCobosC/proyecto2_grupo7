9baa2d3503b4bc4ed05f4c7b9cb619a2c64fee9e
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-14 20:17:32 -0500

    tratando problemas

--- src/main/java/modelo/Admin.java
+++ src/main/java/modelo/Admin.java
@@ -7,4 +7,5 @@ public class Admin extends Usuario {
     public Admin(String usuario, String clave, String nombre, TipoUsuario tipoUsuario) {
         super(usuario,clave,nombre,tipoUsuario); 
     }
+    
 }
--- src/main/java/modelo/TipoUsuario.java
+++
@@ -1,5 +0,0 @@
-package modelo;
-
-public enum TipoUsuario {
-    ADMIN,TECNICO,COBRANZAS
-}
