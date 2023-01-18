616216eab85598938341b7b453a28b69be3484e8
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-17 20:50:19 -0500

    editando ventana de juegoMemoria

--- src/main/java/javafxbase/JuegoMemoriaController.java
+++ src/main/java/javafxbase/JuegoMemoriaController.java
@@ -3,13 +3,16 @@ package javafxbase;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -17,6 +20,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import modelo.*;
 
 
@@ -38,6 +42,10 @@ public class JuegoMemoriaController implements Initializable {
     private HBox menuPanel;
     @FXML
     private Button start;
+    @FXML
+    private Label tiempo;
+    @FXML
+    private Label cantidadAciertos;
     
     @Override
     public void initialize(URL url, ResourceBundle rb){
@@ -49,7 +57,7 @@ public class JuegoMemoriaController implements Initializable {
             this.board.llenandoGridPane();
             for(int row=0; row<4; row++){
                 for(int col=0; col<4; col++){
-                    FileInputStream input = new FileInputStream("src/main/resources/archivos/pregunta.png");
+                    FileInputStream input = new FileInputStream("src/main/resources/imagenes/pregunta.png");
                     Image image = new Image(input);
                     ImageView imageView = new ImageView(image);
                     imageView.setFitWidth(110);
@@ -77,7 +85,7 @@ public class JuegoMemoriaController implements Initializable {
         int rowSeleccionada = Integer.parseInt(posicion.split(",")[0]);
         int colSeleccionada = Integer.parseInt(posicion.split(",")[1]);
         String imagen = board.board[rowSeleccionada][colSeleccionada].value;
-        FileInputStream input = new FileInputStream("src/main/resources/archivos/"+imagen+".png");
+        FileInputStream input = new FileInputStream("src/main/resources/imagenes/"+imagen+".png");
         
         Image imagenSelecionada = new Image(input);
         ((ImageView)node).setImage(imagenSelecionada);
@@ -107,8 +115,8 @@ public class JuegoMemoriaController implements Initializable {
                 ((ImageView)gameMatrix.getChildren().get(indiceSegundaCarta)).setOnMouseClicked(null);
                 
             }else{
-                FileInputStream input = new FileInputStream("src/main/resources/archivos/pregunta.png");
-                FileInputStream input2 = new FileInputStream("src/main/resources/archivos/pregunta.png");
+                FileInputStream input = new FileInputStream("src/main/resources/imagenes/pregunta.png");
+                FileInputStream input2 = new FileInputStream("src/main/resources/imagenes/pregunta.png");
                 
                 int indicePrimeraCarta = (primeraCarta.row * 4) + primeraCarta.col;
                 ((ImageView)gameMatrix.getChildren().get(indicePrimeraCarta)).setImage(new Image(input));
@@ -125,6 +133,7 @@ public class JuegoMemoriaController implements Initializable {
     }  
 
     @FXML
-    private void volver(ActionEvent event) {
+    private void volver(ActionEvent event) throws IOException {
+        App.setRoot("generarOrden");
     }
 }
