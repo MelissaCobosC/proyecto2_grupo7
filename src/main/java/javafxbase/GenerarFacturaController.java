5e5a7d6d443e64b425faf57f894e1532fdd1d56b
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-20 21:33:25 -0500

    terminando generar Factura

--- src/main/java/javafxbase/GenerarFacturaController.java
+++ src/main/java/javafxbase/GenerarFacturaController.java
@@ -46,13 +46,14 @@ public class GenerarFacturaController implements Initializable {
     @FXML
     private TableColumn<Orden, Double> colTotal;
     @FXML
-    private TextField totalPagar;
+    private Label totalPagar;
     @FXML
     private Label empresa;
     @FXML
     private Label periodo;
 
     ArrayList<Cliente> clientes = cargarLista3();
+
     /**
      * Initializes the controller class.
      */
@@ -86,26 +87,26 @@ public class GenerarFacturaController implements Initializable {
         List<Orden> ordenes = Orden.cargarLista();
         int anio = Integer.parseInt(this.anio.getText());
         int mes = Integer.parseInt(this.mes.getText());
-        
+
         List<Orden> ordenesEncontradas = new ArrayList<>();
-        
-        for(Orden orden : ordenes){
+        Double pagoFinal = 50.0;
+        for (Orden orden : ordenes) {
             final boolean cumpleCodigo = codigo.getText().equals(orden.getCodigoCliente());
             final boolean cumpleAnio = anio == orden.getAnio();
             final boolean cumpleMes = mes == orden.getMes();
-            
-            
-            if (cumpleCodigo && cumpleAnio && cumpleMes){
-                ordenesEncontradas.add(orden);
-                periodo.setText(Month.of(mes).toString()+" "+Integer.toString(anio));
-                for(Cliente c:clientes){
-                    if(orden.getCodigoCliente().equals(c.getCodigo())){
-                    empresa.setText(c.getNombre());
+                if (cumpleCodigo && cumpleAnio && cumpleMes) {
+                    pagoFinal += orden.getTotal();
+                    ordenesEncontradas.add(orden);
+                    periodo.setText(Month.of(mes).toString() + " " + Integer.toString(anio));
+                    for (Cliente c : clientes) {
+                        if (orden.getCodigoCliente().equals(c.getCodigo())) {
+                            empresa.setText(c.getNombre());
                     }
-            }
+                }
             }
         }
-        
+        totalPagar.setText(Double.toString(pagoFinal));
+
         this.tablaFactura.getItems().setAll(ordenesEncontradas);
     }
 
--- src/main/java/modelo/Servicio.java
+++ src/main/java/modelo/Servicio.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import static modelo.Orden.cargarLista;
 
 
 public class Servicio {
