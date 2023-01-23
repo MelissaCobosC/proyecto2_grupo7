1d1671168e5117b9cc7a2f574af93beaff0a0784
Melissa Cobos <mncobos@espol.edu.ec>
2023-01-22 22:18:09 -0500

    creando el .jar

---
+++ src/main/java/javafxbase/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxbase;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Principal {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+    
+}
